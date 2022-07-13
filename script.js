8676e3487a6c156fd384696ca838aeb08b0999d3
Pantielieieva <anna.pantielieieva@gmail.com>
2022-07-13 08:42:58 +0100

    Try number 5

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ form.addEventListener("submit", citySearch);
 function showWeather(response) {
   console.log(response.data);
   let temperature = document.querySelector(".temperature");
-    temperature.innerHTML = Math.round(response.data.main.temp);
-    let feelsLike = document.querySelector("#feels-like");
-    feelsLike.innerHTML = Math.round(response.data.main.feels_like);
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  let feelsLike = document.querySelector("#feels-like");
+  feelsLike.innerHTML = Math.round(response.data.main.feels_like);
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = `${response.data.main.humidity}%`;
-    let wind = document.querySelector("#wind");
-    wind.innerHTML = `${response.data.wind.speed}`;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = `${response.data.wind.speed}`;
 
   let description = document.querySelector(".weatherDescription");
   description.innerHTML = `${response.data.weather[0].description}`;
